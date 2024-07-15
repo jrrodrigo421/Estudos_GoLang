6bb7f385a3c7458967aa7655985fa77c200ff1cf
jrrodrigo42 <rjr89000@gmail.com>
2024-07-15 20:24:05 -0300

    feat: variavel reduzida

--- hello.go
+++ hello.go
@@ -7,10 +7,12 @@ import (
 func main() {
 
 	var name string = "Rodrigo"
+	name2 := "Lopes"
+	var teste = "teste"
 	var numero float32 = 11515515151515115151
 	var numero2 float64 = 11515515151515115151
 	var numero3 int = 1
 
 	println(numero)
-	fmt.Println("hello word", name, "\n", numero, "\n", numero2, "\n", numero3)
+	fmt.Println("hello word", name, "\n", numero, "\n", numero2, "\n", numero3, "\n", teste, "\n", name2)
 }
